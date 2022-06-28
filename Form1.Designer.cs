4894b8c3aab7051059e7239b2294657787a54454
Grzegorz Galas <grzegorzgalas10@gmail.com>
2022-06-28 23:07:53 +0200

    Add all panel and icons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HomeBtn = new System.Windows.Forms.Button();
+            this.ValBtn = new System.Windows.Forms.Button();
+            this.ProdBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.ProdBtn);
+            this.panel1.Controls.Add(this.ValBtn);
+            this.panel1.Controls.Add(this.HomeBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // HomeBtn
+            // 
+            this.HomeBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.HomeBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.HomeBtn.BackColor = System.Drawing.Color.DimGray;
+            this.HomeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeBtn.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HomeBtn.ForeColor = System.Drawing.Color.White;
+            this.HomeBtn.Image = ((System.Drawing.Image)(resources.GetObject("HomeBtn.Image")));
+            this.HomeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeBtn.Location = new System.Drawing.Point(166, 0);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(150, 50);
+            this.HomeBtn.TabIndex = 0;
+            this.HomeBtn.Text = "Start";
+            this.HomeBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.HomeBtn.UseVisualStyleBackColor = false;
+            // 
+            // ValBtn
+            // 
+            this.ValBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ValBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ValBtn.BackColor = System.Drawing.Color.DimGray;
+            this.ValBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ValBtn.Font = new System.Drawing.Font("Palatino Linotype", 14.25F);
+            this.ValBtn.ForeColor = System.Drawing.Color.White;
+            this.ValBtn.Image = ((System.Drawing.Image)(resources.GetObject("ValBtn.Image")));
+            this.ValBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ValBtn.Location = new System.Drawing.Point(316, 0);
+            this.ValBtn.Name = "ValBtn";
+            this.ValBtn.Size = new System.Drawing.Size(150, 50);
+            this.ValBtn.TabIndex = 1;
+            this.ValBtn.Text = "Wyceny";
+            this.ValBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ValBtn.UseVisualStyleBackColor = false;
+            // 
+            // ProdBtn
+            // 
+            this.ProdBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ProdBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ProdBtn.BackColor = System.Drawing.Color.DimGray;
+            this.ProdBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProdBtn.Font = new System.Drawing.Font("Palatino Linotype", 14.25F);
+            this.ProdBtn.ForeColor = System.Drawing.Color.White;
+            this.ProdBtn.Image = ((System.Drawing.Image)(resources.GetObject("ProdBtn.Image")));
+            this.ProdBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ProdBtn.Location = new System.Drawing.Point(466, 0);
+            this.ProdBtn.Name = "ProdBtn";
+            this.ProdBtn.Size = new System.Drawing.Size(150, 50);
+            this.ProdBtn.TabIndex = 2;
+            this.ProdBtn.Text = "Produkty";
+            this.ProdBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ProdBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 511);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu główne";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button HomeBtn;
+        private System.Windows.Forms.Button ProdBtn;
+        private System.Windows.Forms.Button ValBtn;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
---
+++ Window/StartPanel.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace AlDar_1._0.Window
+{
+    partial class StartPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartPanel));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(354, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 87);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start ";
+            // 
+            // StartPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 511);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(784, 511);
+            this.Name = "StartPanel";
+            this.Text = "StartPanel";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
